af498135593f8d66abd76ef5fdfceb2d6f8f87df
inanfatih <mfatihinan@gmail.com>
2019-10-01 20:28:55 -0400

    chatbot2 v2

--- script.js
+++ script.js
@@ -1,41 +1,71 @@
 //object containing inputs and outputs arrays
-let chatInputsOutputs = {
-  inputs: ['Hello', 'How are you?', 'What is your favourite colour?'],
-  outputs: [
-    'Hi',
-    'Great!',
-    "I have so many favorites it's hard to choose one.",
-  ],
-};
-console.log(chatInputsOutputs);
-console.log(chatInputsOutputs.inputs);
-console.log(chatInputsOutputs.outputs);
+let chatInputsOutputs = [
+  {
+    inputs: 'Hello',
+    outputs: ['Hello', 'Hey', 'Greetings'],
+  },
+  {
+    inputs: 'What is your favourite colour?',
+    outputs: ['I am not sure.', 'White', 'Blue'],
+  },
+  {
+    inputs: 'How are you?',
+    outputs: ['Great!', 'Not bad', 'Good'],
+  },
+];
 
-function reply() {
+const randomNuberGenerator = () => Math.floor(Math.random() * 3);
+
+console.log(chatInputsOutputs);
+
+const answerRandom = item => {
+  return item[0].outputs[randomNuberGenerator()];
+};
+const answerShortest = item => {
+  return item[0].outputs.reduce((a, b) => (a.length < b.length ? a : b));
+};
+const answerLongest = item => {
+  return item[0].outputs.reduce((a, b) => (a.length > b.length ? a : b));
+};
+
+// reply function
+const reply = selectedAnswer => {
   let question = document.getElementById('input').value.toLowerCase();
 
   console.log(question);
+  document.getElementById('output').value +=
+    'You: ' + document.getElementById('input').value + '\n';
+  const filteredObject = chatInputsOutputs.filter(
+    item => question === item.inputs.toLowerCase(),
+  );
+  console.log('filteredobject', filteredObject);
 
-  // statement to answer the question based on the index of the question.
-  if (
-    question.toLowerCase() &&
-    chatInputsOutputs.inputs
-      .map(v => v.toLowerCase())
-      .includes(question.toLowerCase())
-  ) {
-    let indexOfQuestion = chatInputsOutputs.inputs
-      .map(v => v.toLowerCase())
-      .indexOf(question.toLowerCase());
-    document.getElementById('output').value =
-      chatInputsOutputs.outputs[indexOfQuestion];
+  if (filteredObject.length === 1) {
+    if (selectedAnswer === 1) {
+      document.getElementById('output').value +=
+        'Computer: ' + answerLongest(filteredObject) + '\n\n';
+    } else if (selectedAnswer === 2) {
+      document.getElementById('output').value +=
+        'Computer: ' + answerShortest(filteredObject) + '\n\n';
+    } else {
+      document.getElementById('output').value +=
+        'Computer: ' + answerRandom(filteredObject) + '\n\n';
+    }
   } else {
     //when the input is not recognized
-    document.getElementById('output').value =
-      "I don't understand that command. Please enter another.";
+    document.getElementById('output').value +=
+      "Computer: I don't understand that command. Please enter another. \n\n";
   }
-}
+};
 
+// Event listener of submit button
 document.getElementById('submit').addEventListener('click', function() {
+  let option = 0;
+  if (document.getElementById('longest').checked) {
+    option = 1;
+  } else if (document.getElementById('shortest').checked) {
+    option = 2;
+  }
   console.log('submit clicked');
-  reply();
+  reply(option);
 });
